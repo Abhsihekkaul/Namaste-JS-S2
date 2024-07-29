016deb61d438f9e569bcfb3a1c6b6f72e462e5ef
Abhishek kaul <Abhishekkaul32@gmail.com>
2024-07-29 09:37:31 +0530

    Learned about the callbacks

---
+++ callBacks1.js
@@ -0,0 +1,28 @@
+// A callback is a function passed as an argument to another function
+//This technique allows a function to call another function
+//A callback function can run after another function has finished
+
+function myDisplayer(some) {
+    console.log(some);
+  }
+  
+  function myCalculator(num1, num2) {
+    let sum = num1 + num2;
+    myDisplayer(sum);
+  }
+  
+  myCalculator(5, 5);
+
+
+  /*
+Two issues while using callbacks
+
+1 - Callback hell
+When a function is passed as an argument to another function, it becomes a callback function. This process continues and there are many callbacks inside another's Callback function.
+This grows the code horizontally instead of vertically. That mechanism is known as callback hell. 
+
+2 - Inversion of control
+The callback function is passed to another callback, this way we lose the control of our code. We don't know what is happening behind the scene and the program becomes very difficult to maintain. 
+That process is called inversion of control. 
+  
+  */
